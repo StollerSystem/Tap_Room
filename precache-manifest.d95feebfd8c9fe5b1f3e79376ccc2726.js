df34cf478d25a1adf0c2b02e497d8bee826529bd
Ben Stoller <stollersystemsound@gmail.com>
2020-11-30 11:57:28 -0800

    Updates

---
+++ precache-manifest.d95feebfd8c9fe5b1f3e79376ccc2726.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d58866d0e07d43eee80d345b6b2c65c",
+    "url": "/Tap_Room/index.html"
+  },
+  {
+    "revision": "2ccbc6c8eafcba438f94",
+    "url": "/Tap_Room/static/css/main.b4d6c248.chunk.css"
+  },
+  {
+    "revision": "e9aaa8fef6a2be1d0cac",
+    "url": "/Tap_Room/static/js/2.3156f2c7.chunk.js"
+  },
+  {
+    "revision": "286615ff8697b72a0c05",
+    "url": "/Tap_Room/static/js/3.013fc182.chunk.js"
+  },
+  {
+    "revision": "2ccbc6c8eafcba438f94",
+    "url": "/Tap_Room/static/js/main.a6d994a6.chunk.js"
+  },
+  {
+    "revision": "8a57a2f93f77e19afbc9",
+    "url": "/Tap_Room/static/js/runtime-main.9eae785f.js"
+  },
+  {
+    "revision": "0d992982cb6d24301efdd8ce0a4bb01f",
+    "url": "/Tap_Room/static/media/ClipartKey_1391822B.0d992982.png"
+  }
+]);
